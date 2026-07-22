5063b0a8056f3d4ba0beccd324acdc8704a82ae4
Shohan111369 <sarkershohan346@gmail.com>
2026-07-22 20:57:27 +0800

    added again urtl

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
 
 // Middleware
 app.use(express.json());
-const FRONTEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
- const FRONTEND_URL = "https://doctots-appointment-front.vercel.app";
+const FRONTEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://doctors-appointment-front.vercel.app";
 app.use(
   cors({
     origin: FRONTEND_URL,
